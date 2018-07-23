ee673bf59d28b1f327c346aeba3f879763a9e03b
Ruben Arrebola <ruben.arre6@gmail.com>
2018-07-23 21:09:47 +0200

    First ViewModel and first View

---
+++ wpf-mvvm/ViewModels/PresenterViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wpf_mvvm.MvvmFramework;
+using wpf_mvvm.Modules;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace wpf_mvvm.ViewModels
+{
+    public class PresenterViewModel : ObservableObject
+    {
+        private readonly TextConverter _textConverter = new TextConverter(s => s.ToUpper());
+        private string _text;
+        private readonly ObservableCollection<string> _history = new ObservableCollection<string>();
+
+        public string Text
+        {
+            get { return _text; }
+            set
+            {
+                _text = value;
+                base.RaisePropertyChangedEvent("Text");
+            }
+
+        }
+
+        public IEnumerable<string> History
+        {
+            get { return _history; }
+        }
+
+        public ICommand ConvertTextCommand
+        {
+            get { return new DelegateCommand(this.ConvertText); }
+        }
+
+        private void ConvertText()
+        {
+            if (string.IsNullOrWhiteSpace(Text)) return;
+            AddToHistory(_textConverter.ConvertText(Text));
+            Text = String.Empty;
+        }
+
+        private void AddToHistory(string item)
+        {
+            if (!_history.Contains(item))
+                _history.Add(item);
+        }
+    }
+}
